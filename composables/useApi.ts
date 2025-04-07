11379c8d944aea5fe619fd7b26b9ea80fed4571d
Angelica <angelicatogbevi@gmail.com>
2025-04-07 15:21:03 +0000

    useApi.ts update

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,30 +1,3 @@
-// import axios from "axios";
-// // import { useRuntimeConfig } from "#app";
-
-
-// // const config = useRuntimeConfig();
-
-
-
-// const axiosInstance = axios.create({
-//   baseURL: "https://newsapi.org/v2/",
-//   headers: {
-//     Authorization: `Bearer ad67003737fe4f82b1b865525e97658a`, 
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export async function getData(endpoint: string, params = {}) { 
-//   try {
-//     const response = await axiosInstance.get(endpoint, { params });
-//     console.log("Réponse API:", response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Erreur API:", error);
-//     throw error;
-//   }
-// }
-
 import axios from 'axios';
 import { useRuntimeConfig } from '#app';
 
